237a73fe61416cc15e2b1d974d73f64f057ff476
Ryan Brennan <ryanbrennan@Ryans-MacBook-Pro.local>
2018-07-28 16:53:25 -0700

    Added Get request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,36 @@
 var app = {
   init: function() {},
-  send: function(message) { $.ajax({
+  send: function(message) {
+    $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
+      url: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message', data);
-    }
-  })}
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function(){$.ajax({
+    // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages/message',
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });},
+  server: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages/message'
 
 };
 
