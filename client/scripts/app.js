1567b6fc626f5215da66f403d1f747b823f556ef
Ryan Brennan <ryanbrennan@ryans-mbp.gateway.sonic.net>
2018-07-30 10:53:59 -0700

    Fix clearMessage test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,38 +8,64 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        console.log('chatterbox: Message sent', data);
       },
       error: function (data) {
       
-        console.error('chatterbox: Failed to send message', data);
+        console.log('chatterbox: Failed to send message', data);
       }
     });
   },
-  fetch: function() {
+  fetch: function(message) {
     $.ajax({
-    
-      url: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages/message',
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/message',
       type: 'GET',
-      data: JSON.stringify(message),
+      // data: JSON.stringify('username'),
       contentType: 'application/json',
+
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        data.forEach(function(ele) {
+
+        });
+        console.log('chatterbox: Message Fetched', data);
       },
       error: function (data) {
-     
-        console.error('chatterbox: Failed to send message', data);
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message', data);
       }
     });
   },
-  server: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages/message'
+  server: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages/message',
 
+  clearMessages: function() {
+    //deletes div from specrunner.html //NOT index.html
+    $('#chats').empty();
+  },
+  renderMessage: function() {
+
+    $('#chats').prepend('<div>' + message.text + '</div>'); //refactor
+  },
 };
 
+$(document).ready(function() {
+
+  $('#sendBtn').on('click', function() {
+    app.renderMessage();
+    // app.send(message)
+
+  });
+  $( '#fetchBtn' ).on('click', function() {
+    app.fetch(JSON.stringify('B3BVBAAzY'));
+  });
+
+
+});
 
 var message = {
   username: 'Ryan Meng',
   text: 'Testing',
   roomname: 'TBD'
 };
+
 
