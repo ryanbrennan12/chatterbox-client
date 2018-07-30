a94e85cfafb1d01a25bd1d9404b7dd139f597868
Ryan Brennan <ryanbrennan@Ryans-MacBook-Pro.local>
2018-07-30 15:15:05 -0700

    Fix fetch to get most recent

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,34 +1,55 @@
+$(document).ready(function() {
+  $( '#main' ).on('click', function() {
+    console.log('hello');
+  });
+  $( '#sendBtn' ).on('click', function() {
+    app.send(message);
+  });
+  $( '#fetchBtn' ).on('click', function() {
+    app.fetch();
+  });
+});
+
+
+
+var message = {
+  username: 'rmrmrm',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
+
+
 var app = {
   init: function() {},
   send: function(message) {
     $.ajax({
-    
-      url: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages',
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent', data);
+        $( '<p>chatterbox message successfully sent:' + JSON.stringify(message) + '</br></br>receipt: ' + JSON.stringify(data) + '</p>').appendTo( '#chats' );
       },
       error: function (data) {
-      
-        console.log('chatterbox: Failed to send message', data);
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
       }
     });
   },
   fetch: function(message) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/message',
+      url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/messages',
       type: 'GET',
-      // data: JSON.stringify('username'),
+      data: 'order=-createdAt',
       contentType: 'application/json',
-
       success: function (data) {
-        data.forEach(function(ele) {
-
+        data.results.forEach(function(msg) {
+          $( '<p><strong>' + msg.username + ': </strong>' + msg.text + '</p>').appendTo( '#chats' );
         });
-        console.log('chatterbox: Message Fetched', data);
+        console.log(data);
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -36,36 +57,27 @@ var app = {
       }
     });
   },
-  server: 'http://parse.RPT.hackreactor.com/chatterbox/classes/messages/message',
+  server: 'http://parse.rpt.hackreactor.com/chatterbox/classes/messages',
+  // constructResults: function(msg, info){
+  //   $( "<p>Test</p>" ).appendTo( ".chats" );
 
-  clearMessages: function() {
-    //deletes div from specrunner.html //NOT index.html
-    $('#chats').empty();
-  },
-  renderMessage: function() {
-
-    $('#chats').prepend('<div>' + message.text + '</div>'); //refactor
-  },
+  // }
 };
 
-$(document).ready(function() {
-
-  $('#sendBtn').on('click', function() {
-    app.renderMessage();
-    // app.send(message)
-
-  });
-  $( '#fetchBtn' ).on('click', function() {
-    app.fetch(JSON.stringify('B3BVBAAzY'));
-  });
 
 
-});
 
-var message = {
-  username: 'Ryan Meng',
-  text: 'Testing',
-  roomname: 'TBD'
-};
+
+//   clearMessages: function() {
+//     //deletes div from specrunner.html //NOT index.html
+//     $('#chats').empty();
+//   },
+//   renderMessage: function() {
+
+//     $('#chats').prepend('<div>' + message.text + '</div>'); //refactor
+//   },
+// };
+
+
 
 
