648eb321feb2af7ed79b572a10ce5eb95ab440f1
Ryan Brennan <ryanbrennan@Ryans-MacBook-Pro.local>
2018-08-01 14:34:45 -0700

    Fix first Events test

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,18 +1,8 @@
-
 $(document).ready( () => app.init() );
-
-var message = {
-  username: 'Meng and Ryan',
-  text: 'anything',
-  roomname: '4chan'
-};
-
-
-
 var app = {
   init: function() {
     var selected = $( '#roomSelect option:selected' ).text();
-    app.fetch(selected);
+    // app.fetch(selected); //wtf - this breaks first event mocha test
     $( '#sendBtn' ).on('click', function() {
       app.send(message);
     });
@@ -20,21 +10,13 @@ var app = {
       app.fetch(selected);
     });
     $('#roomSelect').on('change', function() {
-      app.clearMessages();
       selected = $( '#roomSelect option:selected' ).text();
       app.fetch(selected);
     });
-    $('#submission').on('submit', function() {
-      var myMesaage = $('#message').val();
-    
-    });
-    $('#sendMssg').on('click', function() {
-      var input = $('#inputMsg').val();
-      
-      message.text = input;
-      console.log(message);
-      app.send(message);
-      app.fetch();
+    $('#send').on('submit', function(event) { //NEEDS TO BE #send .submit instead of just #send for mocha test
+      event.preventDefault();
+      var message = $('#message').val();
+      app.handleSubmit(message);
     });
     $(document).on('click', '.username', function(event) { app.handleUsernameClick(event); });
   },
@@ -45,7 +27,7 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        app.renderMessage(message);
+        console.log('success');
       },
       error: function (data) {
         console.error('chatterbox: Failed to send message', data);
@@ -53,53 +35,22 @@ var app = {
     });
   },
   fetch: function(selection) {
+    app.clearMessages();
     $.ajax({
-      // This is the url you should use to communicate with the parse API server.
       url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/messages',
       type: 'GET',
       data: { order: '-createdAt'},
       contentType: 'application/json',
       success: function (data) {
-        var date;
-        data.results.forEach(function(msg) {
-          date = new Date (msg.updatedAt);
-          date = date.getMonth() + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
-          if (selection === 'undefined' && !msg.roomname) {
-            $('<p class = \'message\'></p>').appendTo('#chats');
-            $('.message').last().text(function() {
-              return `${date} `;
-            });
-            $('.message').append('<span class = \'username\'></span><span class = \'text\'></span>');
-            $('.username').last().text(function() {
-              return msg.username;
-            });
-            $('.text').last().text(function() {
-              return JSON.stringify(msg.text);
-            });
-          } else if (selection === msg.roomname) {
-            $('<p class = \'message\'></p>').appendTo('#chats');
-            $('.message').last().text(function() {
-              return `${date} `;
-            });
-            $('.message').append('<span class = \'username\'></span><span class = \'text\'></span>');
-            $('.username').last().text(function() {
-              return `${msg.username}`;
-            });
-            $('.text').last().text(function() {
-              return `: ${msg.text}`;
-            });
+        app.clearMessages();
+        data.results.forEach(app.renderMessage);
+        $('option').not(`option:contains(${selection})`).remove(); //remove all rooms except for currently selected
+        var roomnames = _.uniq(data.results.map(message => message.roomname));
+        for (var room of roomnames) {
+          if (room != selection) {
+            app.renderRoom(room, selection);
           }
-        });
-        $('option').not(`option:contains(${selection}), option:contains("undefined") `).remove();
-        var roomnames = [];
-        data.results.forEach(function(msg) {
-          roomnames.push(msg.roomname);
-        });
-        _.uniq(roomnames).forEach(function(rmName) {
-          if (rmName && rmName !== selection) {
-            app.renderRoom(rmName);
-          }
-        });
+        }
       },
       error: function (data) {
         console.error('chatterbox: Failed to fetch message', data);
@@ -108,41 +59,46 @@ var app = {
   },
   server: 'http://parse.rpt.hackreactor.com/chatterbox/classes/messages',
   clearMessages: function() {
-    $( '#chats' ).empty(); 
+    $( '#chats' ).empty();
   },
   renderMessage: function(message) {
-    $( '<p>chatterbox message successfully sent:' + JSON.stringify(message) + '</p>').appendTo( '#chats' );
+    date = new Date (message.updatedAt);
+    date = (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes();
+    $('<p class = \'fetchedMessage\'></p>').appendTo('#chats'); //add 'fetchedMessage' class to #chats
+    $('.fetchedMessage').last().text(function() { //find last 'fetchedMessage' class and add the date
+      return `${date} `;
+    });
+    $('.fetchedMessage').last().append('<span class = \'username\'></span><span class = \'text\'></span>'); //find last 'fetchedMessage' class and add the username class
+    $('.username').last().text(function() {
+      return message.username; //add the username
+    });
+    $('.text').last().text(function() {
+      return JSON.stringify(message.text); //add the message text
+    });
   },
-  renderRoom: function(newRoom) {
-    $('#roomSelect').append('<option></option>');
+  renderRoom: function(newRoom, selection) {
+    // if(!newRoom){
+    //   console.log('false');
+    // }
+    $('#roomSelect').last().append('<option></option>');
     $('option').last().text(newRoom);
   },
   handleUsernameClick: function(event) {
     var friends = [];
     friends.push($(event.target).text());
-    console.log('helloss');
-    return friends;
+    console.log('friend added');
   },
-  handleSubmit: function() {}
+  handleSubmit: function(newMessage) {
+    var message = {
+      username: 'rmrmrm',
+      text: 'trolley',
+      roomname: '4chan'
+    };
+    message.text = newMessage;
+    app.send(message);
+    app.fetch();
+  }
 };
-
-
-
-
-
-
-
-
-//   clearMessages: function() {
-//     //deletes div from specrunner.html //NOT index.html
-//     $('#chats').empty();
-//   },
-//   renderMessage: function() {
-
-//     $('#chats').prepend('<div>' + message.text + '</div>'); //refactor
-//   },
-// };
-
 
 
 
